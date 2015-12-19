e17ce87ca6d6910e73d7c9bcb7653bfd6dd0838e
Nicolas Rakover <nrakover@gmail.com>
2015-12-19 04:07:50 -0500

    cleaning up test script. putting all word detectors into a utility script

---
+++ project/pretrained-word-models.lua
@@ -0,0 +1,75 @@
+require 'svm';
+dofile('data-to-svmlight.lua' );
+dofile('train-test-split.lua' );
+
+
+function getClassifier(dataset)
+	local training_data = torch.load(dataset);
+	local train_full = t7ToSvmlight(training_data.data, training_data.label);
+	local classifier = liblinear.train(train_full, '-s 0 -q');
+	local labels,accuracy,prob = liblinear.predict(train_full, classifier, '-b 1');
+	return classifier
+end
+
+-- train, test = getTrainTestSplit(detector_training_data, 0.9);
+-- train_d = t7ToSvmlight(train.data, train.label);
+-- test_d = t7ToSvmlight(test.data, test.label);
+-- classifier = liblinear.train(train_d, '-s 0 -q');
+-- labels,accuracy,prob = liblinear.predict(test_d, classifier, '-b 1');
+
+
+-- ##################
+-- ##	  NOUNS	   ##
+-- ##################
+function getPersonDetector()
+	local person_classifier = getClassifier('datasets/VOC2007/person_dataset.t7')
+	return {emissions={person_classifier}, transitions=torch.ones(1,1), priors=torch.ones(1)}
+end
+
+function getCarDetector()
+	local car_classifier = getClassifier('datasets/VOC2007/car_dataset.t7')
+	return {emissions={car_classifier}, transitions=torch.ones(1,1), priors=torch.ones(1)}
+end
+
+
+-- ##################
+-- ##	ADJECTIVES ##
+-- ##################
+function getBlackDetector()
+	local black_classifier = getClassifier('datasets/imagenet/attributes_datasets/black.t7')
+	return {emissions={black_classifier}, transitions=torch.ones(1,1), priors=torch.ones(1)}
+end
+
+function getYellowDetector()
+	local yellow_classifier = getClassifier('datasets/imagenet/attributes_datasets/yellow.t7')
+	return {emissions={yellow_classifier}, transitions=torch.ones(1,1), priors=torch.ones(1)}
+end
+
+function getWhiteDetector()
+	local white_classifier = getClassifier('datasets/imagenet/attributes_datasets/white.t7')
+	return {emissions={white_classifier}, transitions=torch.ones(1,1), priors=torch.ones(1)}
+end
+
+
+-- ##################
+-- ##	  VERBS	   ##
+-- ##################
+function getApproachDetector() -- implement
+	-- long-range detector
+	-- mid-range detector
+	-- close-range detector
+
+	-- transition probabilities
+
+	-- prior distribution
+end
+
+
+
+
+
+
+
+
+
+
--- test-tracker.lua
+++ test-tracker.lua
@@ -1,47 +1,22 @@
-require 'svm';
-dofile('data-to-svmlight.lua' );
-dofile('train-test-split.lua' );
-
--- person_training_data = torch.load('datasets/VOC2007/person_dataset.t7');
-car_training_data = torch.load('datasets/VOC2007/car_dataset.t7');
-black_training_data = torch.load('datasets/imagenet/attributes_datasets/black_FIRST_HALF.t7');
-
-
--- train, test = getTrainTestSplit(detector_training_data, 0.9);
--- train_d = t7ToSvmlight(train.data, train.label);
--- test_d = t7ToSvmlight(test.data, test.label);
--- classifier = liblinear.train(train_d, '-s 0 -q');
--- labels,accuracy,prob = liblinear.predict(test_d, classifier, '-b 1');
-
--- train_full = t7ToSvmlight(person_training_data.data, person_training_data.label);
--- person_classifier = liblinear.train(train_full, '-s 0 -q');
--- labels,accuracy,prob = liblinear.predict(train_full, person_classifier, '-b 1');
-
-train_full = t7ToSvmlight(car_training_data.data, car_training_data.label);
-car_classifier = liblinear.train(train_full, '-s 0 -q');
-labels,accuracy,prob = liblinear.predict(train_full, car_classifier, '-b 1');
-
-train_full = t7ToSvmlight(black_training_data.data, black_training_data.label);
-black_classifier = liblinear.train(train_full, '-s 0 -q');
-labels,accuracy,prob = liblinear.predict(train_full, black_classifier, '-b 1');
-
-
+dofile('project/pretrained-word-models.lua');
 
 word_models = {}
-word_models['car'] = {emissions={car_classifier}, transitions=torch.ones(1,1), priors=torch.ones(1)}
-word_models['black'] = {emissions={black_classifier}, transitions=torch.ones(1,1), priors=torch.ones(1)}
+word_models['car'] = getCarDetector()
+word_models['black'] = getBlackDetector()
+word_models['yellow'] = getYellowDetector()
+word_models['white'] = getWhiteDetector()
 
 sentence = {}
-sentence[1] = {word='black', roles={1}}
+sentence[1] = {word='yellow', roles={1}}
 sentence[2] = {word='car', roles={1}}
 
 
 dofile('project/sentence-hmm.lua');
 dofile('track-to-mat.lua')
-sentence = SentenceTracker:new(sentence, 'script_in/black-lambo1.mat', 'script_in/black-lambo1_features.t7', 'script_in/black-lambo1_opticalflow.t7', word_models)
+sentence = SentenceTracker:new(sentence, 'script_in/yellow-white-cars.mat', 'script_in/yellow-white-cars_features.t7', 'script_in/yellow-white-cars_opticalflow.t7', word_models)
 track = sentence:getBestTrack()
 
-trackToMat(track, 'script_out/black-lambo1-BLACK_CAR_TRACK.mat')
+trackToMat(track, 'script_out/yellow-white-cars-YELLOW_CAR_TRACK.mat')
 
 
 -- require 'nn';
