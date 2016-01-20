42a698b3a4ac9968558522a3f7776e31feec8f11
Nicolas Rakover <nrakover@gmail.com>
2016-01-20 15:29:20 -0500

    getTrainTestSplit now splits the positive and negative examples separately

--- track-to-mat.lua
+++ track-to-mat.lua
@@ -2,9 +2,13 @@ require 'torch'
 local matio = require 'matio'
 
 function trackToMat(track, outfile)
-	track_m = {}
+	local track_m = {}
 	for i = 1, #track do
-		track_m['t'..i] = track[i]
+		local tensor_i = torch.Tensor(#track[i], 4)
+		for r = 1, #track[i] do
+			tensor_i[{{r},{}}] = track[i][r]
+		end
+		track_m['t'..i] = tensor_i
 	end
 	track_m['nFrames'] = torch.IntTensor({#track})
 	matio.save(outfile, track_m)
